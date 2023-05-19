957bb678b4c04002b1a3dbe731dd5fbed399e738
Daniela Correa <correadaniela783@gmail.com>
2023-05-19 15:59:22 -0500

    add-img-fond-location

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -5,7 +5,7 @@ const LocationInfo = ({ location }) => {
   return (
     <section className="location">
       <div className="location-container">
-        <h2 className="location-name">{location?.name}</h2>
+        <h2 className="location-name">{ location?.name}</h2>
         <ul className="location-list">
           <li className="location-item">
             <span className="location-label">Type :</span>
